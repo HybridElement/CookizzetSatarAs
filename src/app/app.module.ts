a41ceaa20040fbaa9f88260c2838969c8d609e3e
Hybrid <32874500+HybridElement@users.noreply.github.com>
2018-03-20 09:32:18 +0100

    180320

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PodcastPage } from '../pages/podcast/podcast';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PodcastPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PodcastPage
   ],
   providers: [
     StatusBar,
